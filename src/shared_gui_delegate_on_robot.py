0f1c2552b03a5b98a1dfef206651901fd7fc9e4f
conradja <conradja@rose-hulman.edu>
2019-02-06 10:38:57 -0500

    Added my Name to collaborators list

--- src/shared_gui_delegate_on_robot.py
+++ src/shared_gui_delegate_on_robot.py
@@ -3,7 +3,7 @@
   This code is the delegate for handling messages from the shared GUI.
 
   Author:  Your professors (for the framework)
-    and PUT_YOUR_NAMES_HERE.
+    and Joseph Conrad, Derrick Swart, Joshua Giambattista.
   Winter term, 2018-2019.
 """
 
