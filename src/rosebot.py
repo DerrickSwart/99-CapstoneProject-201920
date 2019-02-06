5612aa1203999fba97c32fce5a9c00d04ea3f26f
conradja <conradja@rose-hulman.edu>
2019-02-06 11:10:38 -0500

    Wrote Calibrate Arm

--- src/rosebot.py
+++ src/rosebot.py
@@ -215,6 +215,7 @@ class ArmAndClaw(object):
         while 14.2*360 > abs(position):
             position = self.motor.get_position()
         self.motor.turn_off()
+        self.motor.reset_position()
     def move_arm_to_position(self, desired_arm_position):
         """
         Move its Arm to the given position, where 0 means all the way DOWN.
@@ -226,6 +227,7 @@ class ArmAndClaw(object):
         Lowers the Arm until it is all the way down, i.e., position 0.
         The robot must have previously calibrated its Arm.
         """
+
 
 ###############################################################################
 #    SensorSystem
