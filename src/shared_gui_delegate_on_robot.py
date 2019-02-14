d2435ad126120cf4ad7d72d49eb1a1eddd9e0ab5
swartdc <swartdc@rose-hulman.edu>
2019-02-13 21:15:46 -0500

    appended m1 feature 10 to implement another section that has been finished

--- src/shared_gui_delegate_on_robot.py
+++ src/shared_gui_delegate_on_robot.py
@@ -106,6 +106,8 @@ class Handler(object):
                     break
         if function == 'beep':
             self.m1_pick_up_using_prox(float(initial_value), float(rate_entry))
+        if function == 'LED':
+            self.m3_grab_object_with_LED(int(initial_value), float(rate_entry))
 
 
 
